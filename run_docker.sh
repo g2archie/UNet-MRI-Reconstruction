231662d6cd6ebe9816728907a162e26f5e88ebb4
Wentao Wei <wentao.wei.15@ucl.ac.uk>
2019-08-27 06:28:06 +0100

    updated jupyter notebooks and images

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,9 @@
 #!/bin/bash 
 
-
-SOURCE='/home/wentao/tensorflow_serving_model/models/UNet2D2D'
-TARGET='/models/UNet2D2D'
 MODEL_NAME='UNet2D2D'
+SOURCE='/home/wentao/tensorflow_serving_model/models/'$MODEL_NAME
+TARGET='/models/'$MODEL_NAME
 
-
-docker run --runtime=nvidia -p 8501:8501 --mount type=bind,source=$SOURCE,target=$TARGET -e MODEL_NAME=$MODEL_NAME -t tensorflow/serving:latest-gpu &
-
-
-
+docker run --runtime=nvidia -p 8501:8501 \
+--mount type=bind,source=$SOURCE,target=$TARGET -e\
+MODEL_NAME=$MODEL_NAME -t tensorflow/serving:latest-gpu &
